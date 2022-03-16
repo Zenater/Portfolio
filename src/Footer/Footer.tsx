57a7099ad6db7b54281e0d08055f335e4d5d07f0
Zenater <zent230096@mail.ru>
2022-03-16 15:44:18 +0300

    added footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import f from './Footer.module.css'
 import b from "../common/styles/Container.module.css";
-import s from "../skils/Skills.module.css";
 
 
 const Footer = () => {
     return (
         <div className={f.footerBlock}>
-            <div className={b.container}>
-                <div className={f.footerContainer}>
-                <h2 className={s.title}>Vitaliy Volkov</h2>
-            <div>
-                <div className={f.footerSocialContainer}></div>
-                <div className={f.icon}></div>
-                <div className={f.icon}></div>
-                <div className={f.icon}></div>
-                <div className={f.icon}></div>
-            </div>
-            </div>
+            <div className={`${b.container} ${f.footerContainer}`}>
+                <h2 className={f.title}>Vitaliy Volkov</h2>
+                <div className={f.iconContainer}>
+                    <div className={f.icon}>1</div>
+                    <div className={f.icon}>2</div>
+                    <div className={f.icon}>3</div>
+                    <div className={f.icon}>4</div>
+                </div>
+                <div>2019 все права защищены</div>
             </div>
         </div>
+
     );
 };
 
